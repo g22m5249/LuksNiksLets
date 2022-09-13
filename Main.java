2809eba9ec31a94260bed53fd43921dee935fc3f
Nikiwe <g22M7353@campus.ru.ac.za>
2022-09-13 19:24:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Tictactoe tictactoe = new Tictactoe(System.println"Hey guys @Lukhanyo please don't forget to comment the code so that I can be able to do the design");
+		Tictactoe tictactoe = new Tictactoe()
+		System.println("Hey guys @Lukhanyo please don't forget to comment the code so that I can be able to do the design");
 	}
 
 }
