aaa871b4ebab86c6f69e57aa10294325ae61a647
Nikiwe <g22M7353@campus.ru.ac.za>
2022-10-11 21:58:46 +0200

    Update MainMenu.java

--- TicTacToe/app/src/main/java/com/example/tic_tac_toe/MainMenu.java
+++ TicTacToe/app/src/main/java/com/example/tic_tac_toe/MainMenu.java
@@ -1,8 +1,11 @@
-package com.example.tic_tac_toe;
+package com.example.clickonxno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button buttonMenu = findViewById(R.id.button_multi);
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonCPU= findViewById(R.id.button_cpu);
+        buttonCPU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenu.this, Multiplayer.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
