f66655709c7bd7d374a72b87be9b142d18ce916a
Nikiwe <g22M7353@campus.ru.ac.za>
2022-10-11 21:56:02 +0200

    Update Backsplash.java

--- TicTacToe/app/src/main/java/com/example/tic_tac_toe/Backsplash.java
+++ TicTacToe/app/src/main/java/com/example/tic_tac_toe/Backsplash.java
@@ -1,8 +1,10 @@
-package com.example.tic_tac_toe;
+package com.example.clickonxno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Backsplash extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Backsplash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backsplash);
+
+        Intent main = new Intent(Backsplash.this, MainMenu.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(main);
+            }
+        }, 4000);
     }
 }
